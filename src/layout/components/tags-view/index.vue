eccf1e1c130d6ae5d395636da2989a69b0007d39
rookie <1764600907@qq.com>
2025-02-28 11:24:06 +0800

    feat: 新增了标签栏的右键点击展示更多操作的菜单功能

--- src/layout/components/tags-view/index.vue
+++ src/layout/components/tags-view/index.vue
@@ -23,168 +23,294 @@ const isActive = (tag) => tag.path === route.path
 const isAffix = (tag) => tag?.affix
 
 const toLastView = (visitedViews, view) => {
-  const latestView = visitedViews.slice(-1)[0]
-  if (latestView && latestView.fullPath) {
-    router.push(latestView.fullPath)
-  } else {
-    if (view?.name === 'home') {
-      router.replace({ path: '/redirect' + view.fullPath })
-    } else {
-      router.push('/')
-    }
-  }
+	const latestView = visitedViews.slice(-1)[0]
+	if (latestView && latestView.fullPath) {
+		router.push(latestView.fullPath)
+	} else {
+		if (view?.name === 'home') {
+			router.replace({ path: '/redirect' + view.fullPath })
+		} else {
+			router.push('/')
+		}
+	}
 }
 
 const closeSelectedTag = (tag) => {
-  // 找出 visitedViews 中重复的视图组件，只删除当前视图，不从缓存中删除组件
-  const result = findDuplicates(visitedViews.value)
-  if (result.length > 0 && result.includes(tag.name)) {
-    tagsViewStore.delVisitedView(tag).then((res) => {
-      if (isActive(tag)) {
-        toLastView(res, tag)
-      }
-    })
-    return
-  }
+	// 找出 visitedViews 中重复的视图组件，只删除当前视图，不从缓存中删除组件
+	const result = findDuplicates(visitedViews.value)
+	if (result.length > 0 && result.includes(tag.name)) {
+		tagsViewStore.delVisitedView(tag).then((res) => {
+			if (isActive(tag)) {
+				toLastView(res, tag)
+			}
+		})
+		return
+	}
 
-  tagsViewStore.delView(tag).then((res) => {
-    // 当前关闭的标签是已经激活的视图，跳转到最后一个视图
-    if (isActive(tag)) {
-      toLastView(res.visitedViews, tag)
-    }
-  })
+	tagsViewStore.delView(tag).then((res) => {
+		// 当前关闭的标签是已经激活的视图，跳转到最后一个视图
+		if (isActive(tag)) {
+			toLastView(res.visitedViews, tag)
+		}
+	})
 }
 
 const filterAffixTags = (routes, basePath = '/') => {
-  const tags = []
-  const processRoute = (route) => {
-    const tagPath = resolve(basePath, route.path)
-    const tag = {
-      path: tagPath,
-      fullPath: tagPath,
-      name: route.name,
-      title: route.meta?.title || 'no-name',
-      affix: route.meta?.affix,
-      keepAlive: route.meta?.keepAlive
-    }
-    if (tag.affix) {
-      tags.push(tag)
-    }
-    if (route.children) {
-      route.children.forEach(processRoute)
-    }
-  }
-  routes.forEach(processRoute)
-  return tags
+	const tags = []
+	const processRoute = (route) => {
+		const tagPath = resolve(basePath, route.path)
+		const tag = {
+			path: tagPath,
+			fullPath: tagPath,
+			name: route.name,
+			title: route.meta?.title || 'no-name',
+			affix: route.meta?.affix,
+			keepAlive: route.meta?.keepAlive
+		}
+		if (tag.affix) {
+			tags.push(tag)
+		}
+		if (route.children) {
+			route.children.forEach(processRoute)
+		}
+	}
+	routes.forEach(processRoute)
+	return tags
 }
 
 function initTags() {
-  const tags = filterAffixTags(routes)
-  affixTags.value = tags
-  for (const tag of tags) {
-    // Must have tag name
-    if (tag.name) {
-      tagsViewStore.addVisitedView(tag)
-    }
-  }
+	const tags = filterAffixTags(routes)
+	affixTags.value = tags
+	for (const tag of tags) {
+		// Must have tag name
+		if (tag.name) {
+			tagsViewStore.addVisitedView(tag)
+		}
+	}
 }
 
 function addTags() {
-  if (route.meta.title) {
-    tagsViewStore.addView(route)
-  }
-  return false
+	if (route.meta.title) {
+		tagsViewStore.addView(route)
+	}
+	return false
 }
 
 async function moveToCurrentTag() {
-  await nextTick()
+	await nextTick()
 
-  const tagList = tags.value.filter(Boolean)
-  for (const tag of tagList) {
-    if (tag.to.path === route.path) {
-      moveToTarget(tag)
-      if (tag.to.fullPath !== route.fullPath) {
-        // query 不同时更新 tag 信息
-        tagsViewStore.updateVisitedView(route)
-      }
-    }
-  }
+	const tagList = tags.value.filter(Boolean)
+	for (const tag of tagList) {
+		if (tag.to.path === route.path) {
+			moveToTarget(tag)
+			if (tag.to.fullPath !== route.fullPath) {
+				// query 不同时更新 tag 信息
+				tagsViewStore.updateVisitedView(route)
+			}
+		}
+	}
 }
 
 function moveToTarget(currentTag) {
-  const $container = scrollContainer.value
-  const $containerWidth = $container.wrapRef.offsetWidth
-  const $scrollWrapper = scrollWrapper.value
-  const tagList = tags.value.filter(Boolean)
+	const $container = scrollContainer.value
+	const $containerWidth = $container.wrapRef.offsetWidth
+	const $scrollWrapper = scrollWrapper.value
+	const tagList = tags.value.filter(Boolean)
 
-  let firstTag = null
-  let lastTag = null
+	let firstTag = null
+	let lastTag = null
 
-  // find first tag and last tag
-  if (tagList.length > 0) {
-    firstTag = tagList[0]
-    lastTag = tagList[tagList.length - 1]
-  }
+	// find first tag and last tag
+	if (tagList.length > 0) {
+		firstTag = tagList[0]
+		lastTag = tagList[tagList.length - 1]
+	}
 
-  if (firstTag.to.path === currentTag.to.path) {
-    $scrollWrapper.scrollLeft = 0
-  } else if (lastTag.to.path === currentTag.to.path) {
-    $scrollWrapper.scrollLeft = $scrollWrapper.scrollWidth - $containerWidth
-  } else {
-    // find preTag and nextTag
-    const currentIndex = tagList.findIndex((item) => item === currentTag)
-    const preTag = tagList[currentIndex - 1]
-    const nextTag = tagList[currentIndex + 1]
+	if (firstTag.to.path === currentTag.to.path) {
+		$scrollWrapper.scrollLeft = 0
+	} else if (lastTag.to.path === currentTag.to.path) {
+		$scrollWrapper.scrollLeft = $scrollWrapper.scrollWidth - $containerWidth
+	} else {
+		// find preTag and nextTag
+		const currentIndex = tagList.findIndex((item) => item === currentTag)
+		const preTag = tagList[currentIndex - 1]
+		const nextTag = tagList[currentIndex + 1]
 
-    // the tag's offsetLeft after of nextTag
-    const afterNextTagOffsetLeft = nextTag.$el.offsetLeft + nextTag.$el.offsetWidth
+		// the tag's offsetLeft after of nextTag
+		const afterNextTagOffsetLeft = nextTag.$el.offsetLeft + nextTag.$el.offsetWidth
 
-    // the tag's offsetLeft before of preTag
-    const beforePreTagOffsetLeft = preTag.$el.offsetLeft + preTag.$el.offsetWidth
+		// the tag's offsetLeft before of preTag
+		const beforePreTagOffsetLeft = preTag.$el.offsetLeft + preTag.$el.offsetWidth
 
-    if (afterNextTagOffsetLeft > $scrollWrapper.scrollLeft + $containerWidth) {
-      $scrollWrapper.scrollLeft = afterNextTagOffsetLeft - $containerWidth
-    } else if (beforePreTagOffsetLeft < $scrollWrapper.scrollLeft) {
-      $scrollWrapper.scrollLeft = beforePreTagOffsetLeft
-    }
-  }
+		if (afterNextTagOffsetLeft > $scrollWrapper.scrollLeft + $containerWidth) {
+			$scrollWrapper.scrollLeft = afterNextTagOffsetLeft - $containerWidth
+		} else if (beforePreTagOffsetLeft < $scrollWrapper.scrollLeft) {
+			$scrollWrapper.scrollLeft = beforePreTagOffsetLeft
+		}
+	}
 }
 
 watch(route, () => {
-  addTags()
-  moveToCurrentTag()
+	addTags()
+	moveToCurrentTag()
 })
 
 onMounted(() => {
-  initTags()
-  addTags()
+	initTags()
+	addTags()
 })
+
+// 右击tag 操作菜单
+const visible = ref(false)
+const left = ref(0)
+const top = ref(0)
+const selectedTag = ref({
+	path: '',
+	fullPath: '',
+	name: '',
+	title: '',
+	affix: false,
+	keepAlive: false
+})
+
+watch(visible, (value) => {
+	if (value) {
+		document.body.addEventListener('click', closeMenu)
+	} else {
+		document.body.removeEventListener('click', closeMenu)
+	}
+})
+
+function openMenu(tag, event) {
+	const el = scrollWrapper.value
+
+	const menuMinWidth = 105
+	const offsetLeft = el.getBoundingClientRect().left // 当前组件左边距离屏幕边缘
+	const offsetWidth = el.offsetWidth // 当前组件宽度
+	const maxLeft = offsetWidth - menuMinWidth // 左边最大范围
+	const l = event.clientX - offsetLeft + 15 // 15 偏移量
+
+	if (l > maxLeft) {
+		left.value = maxLeft
+	} else {
+		left.value = l
+	}
+
+	top.value = event.clientY - 48 // 减去头部高度
+	visible.value = true
+	selectedTag.value = tag
+}
+
+function closeMenu() {
+	visible.value = false
+}
+
+// 关闭其他
+const closeOthersTags = () => {
+	router.push(selectedTag.value)
+	tagsViewStore.delOtherViews(selectedTag.value).then(() => {
+		moveToCurrentTag()
+	})
+}
+// 关闭所有
+function closeAllTags(view) {
+	tagsViewStore.delAllViews().then(({ visitedViews }) => {
+		toLastView(visitedViews, view)
+	})
+}
+
+// 刷新
+const refreshSelectedTag = (view) => {
+	tagsViewStore.delCachedView(view)
+	const { fullPath } = view
+	nextTick(() => {
+		router.replace('/redirect' + fullPath)
+	})
+}
+// 关闭左侧
+const closeLeftTags = () => {
+	tagsViewStore.delLeftViews(selectedTag.value).then((res) => {
+		if (!res.visitedViews.find((item) => item?.path === route?.path)) {
+			toLastView(res.visitedViews)
+		}
+	})
+}
+// 关闭右侧
+const closeRightTags = () => {
+	tagsViewStore.delRightViews(selectedTag.value).then((res) => {
+		if (!res.visitedViews.find((item) => item?.path === route?.path)) {
+			toLastView(res.visitedViews)
+		}
+	})
+}
+
+const handleScroll = () => {
+	closeMenu()
+}
 </script>
 
 <template>
-  <div class="w-full bg-white shadow-sm border-b border-solid border-gray-200">
-    <el-scrollbar ref="scrollContainer">
-      <div class="flex items-center whitespace-nowrap">
-        <router-link
-          v-for="(tag, index) in visitedViews"
-          :ref="(el) => (tags[index] = el)"
-          :key="tag.path"
-          :class="['tags-item', isActive(tag) ? 'active' : '']"
-          :to="{ path: tag.path, query: tag.query }"
-        >
-          <div
-            :style="{ borderColor: isActive(tag) ? 'var(--main-color)' : '' }"
-            class="inline-block flex items-center gap-[12px] h-[46px] px-[16px] box-border border-b-[3px] border-solid"
-          >
-            <span class="text-[14px] leading-[22px] text-[var(--gray-700)]">
-              {{ tag.title }}
-            </span>
-            <el-icon v-if="!isAffix(tag)" @click.prevent.stop="closeSelectedTag(tag)">
-              <Close />
-            </el-icon>
-          </div>
-        </router-link>
-      </div>
-    </el-scrollbar>
-  </div>
+	<div class="w-full bg-white shadow-sm border-b border-solid border-gray-200 relative">
+		<el-scrollbar ref="scrollContainer" @wheel.prevent="handleScroll">
+			<div class="flex items-center whitespace-nowrap">
+				<router-link
+					v-for="(tag, index) in visitedViews"
+					:ref="(el) => (tags[index] = el)"
+					:key="tag.path"
+					:class="['tags-item', isActive(tag) ? 'active' : '']"
+					:to="{ path: tag.path, query: tag.query }"
+					@click.middle="!isAffix(tag) ? closeSelectedTag(tag) : ''"
+					@contextmenu.prevent="openMenu(tag, $event, index)"
+				>
+					<div
+						:style="{ borderColor: isActive(tag) ? 'var(--main-color)' : '' }"
+						class="inline-block flex items-center gap-[12px] h-[46px] px-[16px] box-border border-b-[3px] border-solid"
+					>
+						<span class="text-[14px] leading-[22px] text-[var(--gray-700)]">
+							{{ tag.title }}
+						</span>
+						<el-icon v-if="!isAffix(tag)" @click.prevent.stop="closeSelectedTag(tag)">
+							<Close />
+						</el-icon>
+					</div>
+				</router-link>
+			</div>
+		</el-scrollbar>
+
+		<ul v-show="visible" :style="{ left: left + 'px', top: top + 'px' }" class="contextmenu">
+			<li @click="refreshSelectedTag(selectedTag)">刷新</li>
+			<li v-if="!isAffix(selectedTag)" @click="closeSelectedTag(selectedTag)">关闭</li>
+			<li @click="closeOthersTags">关闭其它</li>
+			<li @click="closeLeftTags(selectedTag)">关闭左侧</li>
+			<li @click="closeRightTags(selectedTag)">关闭右侧</li>
+			<li @click="closeAllTags(selectedTag)">关闭所有</li>
+		</ul>
+	</div>
 </template>
+
+<style scoped lang="scss">
+// 鼠标右键 展示菜单
+.contextmenu {
+	position: absolute;
+	z-index: 999;
+	padding: 5px 0;
+	margin: 0;
+	font-size: 12px;
+	font-weight: 400;
+	color: #333;
+	list-style-type: none;
+	background: #fff;
+	border-radius: 4px;
+	box-shadow: 2px 2px 3px 0 rgb(0 0 0 / 30%);
+
+	li {
+		padding: 7px 16px;
+		margin: 0;
+		cursor: pointer;
+
+		&:hover {
+			background: #eee;
+		}
+	}
+}
+</style>
--- src/stores/tagsView.js
+++ src/stores/tagsView.js
@@ -1,157 +1,199 @@
 export const useTagsViewStore = defineStore(
-  'tags-view',
-  () => {
-    const visitedViews = ref([])
-    const cachedViews = ref([])
+	'tags-view',
+	() => {
+		const visitedViews = ref([])
+		const cachedViews = ref([])
 
-    const addVisitedView = (view) => {
-      //  如果已经存在于访问的视图列表中，则不再添加
-      if (visitedViews.value.some((v) => v.path === view.path)) return
-      // 如果视图是固定的(affix),则在已访问的视图列表的开头添加
-      if (view.affix) {
-        visitedViews.value.unshift(view)
-      } else {
-        visitedViews.value.push(
-          Object.assign({}, view, {
-            title: view.meta.title || 'no-name'
-          })
-        )
-      }
-    }
+		const addVisitedView = (view) => {
+			//  如果已经存在于访问的视图列表中，则不再添加
+			if (visitedViews.value.some((v) => v.path === view.path)) return
+			// 如果视图是固定的(affix),则在已访问的视图列表的开头添加
+			if (view.affix) {
+				visitedViews.value.unshift(view)
+			} else {
+				visitedViews.value.push(
+					Object.assign({}, view, {
+						title: view.meta.title || 'no-name'
+					})
+				)
+			}
+		}
 
-    const addCachedView = (view) => {
-      // 如果已经存在于缓存的视图列表中，则不再添加
-      if (cachedViews.value.includes(view.name)) return
-      // 如果视图需要缓存(keepAlive),则将其路由名称添加到缓存视图列表中
-      if (view.meta.keepAlive) {
-        cachedViews.value.push(view.name)
-      }
-    }
+		const addCachedView = (view) => {
+			// 如果已经存在于缓存的视图列表中，则不再添加
+			if (cachedViews.value.includes(view.name)) return
+			// 如果视图需要缓存(keepAlive),则将其路由名称添加到缓存视图列表中
+			if (view.meta.keepAlive) {
+				cachedViews.value.push(view.name)
+			}
+		}
 
-    const delVisitedView = (view) => {
-      return new Promise((resolve) => {
-        for (const [i, v] of visitedViews.value.entries()) {
-          // 找到与指定视图路径匹配的视图，在已访问视图列表中删除该视图
-          if (v.path === view.path) {
-            visitedViews.value.splice(i, 1)
-            break
-          }
-        }
-        resolve([...visitedViews.value])
-      })
-    }
+		const delVisitedView = (view) => {
+			return new Promise((resolve) => {
+				for (const [i, v] of visitedViews.value.entries()) {
+					// 找到与指定视图路径匹配的视图，在已访问视图列表中删除该视图
+					if (v.path === view.path) {
+						visitedViews.value.splice(i, 1)
+						break
+					}
+				}
+				resolve([...visitedViews.value])
+			})
+		}
 
-    const delCachedView = (view) => {
-      return new Promise((resolve) => {
-        const index = cachedViews.value.indexOf(view.name)
-        index > -1 && cachedViews.value.splice(index, 1)
-        resolve([...cachedViews.value])
-      })
-    }
+		const delCachedView = (view) => {
+			return new Promise((resolve) => {
+				const index = cachedViews.value.indexOf(view.name)
+				index > -1 && cachedViews.value.splice(index, 1)
+				resolve([...cachedViews.value])
+			})
+		}
 
-    const delOtherVisitedViews = (view) => {
-      return new Promise((resolve) => {
-        visitedViews.value = visitedViews.value.filter((v) => {
-          return v?.affix || v.path === view.path
-        })
-        resolve([...visitedViews.value])
-      })
-    }
+		const delOtherVisitedViews = (view) => {
+			return new Promise((resolve) => {
+				visitedViews.value = visitedViews.value.filter((v) => {
+					return v?.affix || v.path === view.path
+				})
+				resolve([...visitedViews.value])
+			})
+		}
 
-    const delOtherCachedViews = (view) => {
-      return new Promise((resolve) => {
-        const index = cachedViews.value.indexOf(view.name)
-        if (index > -1) {
-          cachedViews.value = cachedViews.value.slice(index, index + 1)
-        } else {
-          // if index = -1, there is no cached tags
-          cachedViews.value = []
-        }
-        resolve([...cachedViews.value])
-      })
-    }
+		const delOtherCachedViews = (view) => {
+			return new Promise((resolve) => {
+				const index = cachedViews.value.indexOf(view.name)
+				if (index > -1) {
+					cachedViews.value = cachedViews.value.slice(index, index + 1)
+				} else {
+					// if index = -1, there is no cached tags
+					cachedViews.value = []
+				}
+				resolve([...cachedViews.value])
+			})
+		}
 
-    const updateVisitedView = (view) => {
-      for (let v of visitedViews.value) {
-        if (v.path === view.path) {
-          v = Object.assign(v, view)
-          break
-        }
-      }
-    }
+		const updateVisitedView = (view) => {
+			for (let v of visitedViews.value) {
+				if (v.path === view.path) {
+					v = Object.assign(v, view)
+					break
+				}
+			}
+		}
 
-    const addView = (view) => {
-      addVisitedView(view)
-      addCachedView(view)
-    }
+		const addView = (view) => {
+			addVisitedView(view)
+			addCachedView(view)
+		}
 
-    const delView = (view) => {
-      return new Promise((resolve) => {
-        delVisitedView(view)
-        delCachedView(view)
-        resolve({
-          visitedViews: [...visitedViews.value],
-          cachedViews: [...cachedViews.value]
-        })
-      })
-    }
+		const delView = (view) => {
+			return new Promise((resolve) => {
+				delVisitedView(view)
+				delCachedView(view)
+				resolve({
+					visitedViews: [...visitedViews.value],
+					cachedViews: [...cachedViews.value]
+				})
+			})
+		}
 
-    const delOtherViews = (view) => {
-      return new Promise((resolve) => {
-        delOtherVisitedViews(view)
-        delOtherCachedViews(view)
-        resolve({
-          visitedViews: [...visitedViews.value],
-          cachedViews: [...cachedViews.value]
-        })
-      })
-    }
+		const delOtherViews = (view) => {
+			return new Promise((resolve) => {
+				delOtherVisitedViews(view)
+				delOtherCachedViews(view)
+				resolve({
+					visitedViews: [...visitedViews.value],
+					cachedViews: [...cachedViews.value]
+				})
+			})
+		}
 
-    const delAllCachedViews = () => {
-      return new Promise((resolve) => {
-        cachedViews.value = []
-        resolve([...cachedViews.value])
-      })
-    }
+		function delLeftViews(view) {
+			return new Promise((resolve) => {
+				const currIndex = visitedViews.value.findIndex((v) => v?.path === view?.path)
+				if (currIndex === -1) {
+					return
+				}
+				visitedViews.value = visitedViews.value.filter((item, index) => {
+					if (index >= currIndex || item?.affix) {
+						return true
+					}
 
-    const delAllVisitedViews = () => {
-      return new Promise((resolve) => {
-        const affixTags = visitedViews.value.filter((tag) => tag?.affix)
-        visitedViews.value = affixTags
-        resolve([...visitedViews.value])
-      })
-    }
+					const cacheIndex = cachedViews.value.indexOf(item?.name)
+					if (cacheIndex > -1) {
+						cachedViews.value.splice(cacheIndex, 1)
+					}
+					return false
+				})
+				resolve({
+					visitedViews: [...visitedViews.value]
+				})
+			})
+		}
 
-    const delAllViews = () => {
-      return new Promise((resolve) => {
-        delAllCachedViews()
-        delAllVisitedViews()
-        resolve({
-          visitedViews: [...visitedViews.value],
-          cachedViews: [...cachedViews.value]
-        })
-      })
-    }
+		function delRightViews(view) {
+			return new Promise((resolve) => {
+				const currIndex = visitedViews.value.findIndex((v) => v?.path === view?.path)
+				if (currIndex === -1) {
+					return
+				}
+				visitedViews.value = visitedViews.value.filter((item, index) => {
+					if (index <= currIndex || item?.affix) {
+						return true
+					}
+				})
+				resolve({
+					visitedViews: [...visitedViews.value]
+				})
+			})
+		}
 
-    return {
-      visitedViews,
-      cachedViews,
-      addVisitedView,
-      addCachedView,
-      delVisitedView,
-      delCachedView,
-      delOtherVisitedViews,
-      delOtherCachedViews,
-      updateVisitedView,
-      addView,
-      delView,
-      delOtherViews,
-      delAllCachedViews,
-      delAllVisitedViews,
-      delAllViews
-    }
-  },
-  {
-    persist: true
-  }
+		const delAllCachedViews = () => {
+			return new Promise((resolve) => {
+				cachedViews.value = []
+				resolve([...cachedViews.value])
+			})
+		}
+
+		const delAllVisitedViews = () => {
+			return new Promise((resolve) => {
+				const affixTags = visitedViews.value.filter((tag) => tag?.affix)
+				visitedViews.value = affixTags
+				resolve([...visitedViews.value])
+			})
+		}
+
+		const delAllViews = () => {
+			return new Promise((resolve) => {
+				delAllCachedViews()
+				delAllVisitedViews()
+				resolve({
+					visitedViews: [...visitedViews.value],
+					cachedViews: [...cachedViews.value]
+				})
+			})
+		}
+
+		return {
+			visitedViews,
+			cachedViews,
+			addVisitedView,
+			addCachedView,
+			delVisitedView,
+			delCachedView,
+			delOtherVisitedViews,
+			delOtherCachedViews,
+			delLeftViews,
+			delRightViews,
+			updateVisitedView,
+			addView,
+			delView,
+			delOtherViews,
+			delAllCachedViews,
+			delAllVisitedViews,
+			delAllViews
+		}
+	},
+	{
+		persist: true
+	}
 )
